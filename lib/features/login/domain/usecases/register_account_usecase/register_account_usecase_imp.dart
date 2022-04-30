70dda18cb005a91126b0e3552b29065cd7966b8e
Jordan Elizeu <jordan.elizeu@ilia.digital>
2022-04-30 18:51:09 -0300

    Criando validação de e-mail e finalizando caso de uso - cadastrar conta

--- lib/features/login/domain/usecases/register_account_usecase/register_account_usecase_imp.dart
+++ lib/features/login/domain/usecases/register_account_usecase/register_account_usecase_imp.dart
@@ -1,12 +1,18 @@
 import 'package:controle_financeiro/features/login/domain/entities/register_account_entity.dart';
 import 'package:controle_financeiro/features/login/domain/usecases/register_account_usecase/register_account_usecase.dart';
 import 'package:dartz/dartz.dart';
+import '../../../../../core/validation/email_validation.dart';
 
 class RegisterAccountUseCaseImp implements RegisterAccountUseCase {
   @override
   Future<Either<Exception, RegisterAccountEntity>> registerAccount(
-      {required RegisterAccountEntity registerAccountEntity}) {
-    // TODO: implement registerAccount
-    throw UnimplementedError();
+      {required RegisterAccountEntity registerAccountEntity}) async {
+    if (registerAccountEntity.name.isNotEmpty &&
+        EmailValidation.validateEmail(registerAccountEntity.email) &&
+        registerAccountEntity.password.length >= 6) {
+      return Right(registerAccountEntity);
+    } else {
+      return Left(Exception('erro não esperado'));
+    }
   }
 }
